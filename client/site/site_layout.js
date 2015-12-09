e27ac8cf7887b994578a3ec8fd81940a07068757
rexdiamante28 <rexdiamante28@gmail.com>
2015-12-09 13:43:06 +0800

    committing final changes

--- client/site/site_layout.js
+++ client/site/site_layout.js
@@ -13,10 +13,10 @@ Main  = function main() {
        $(window).scroll(function() {
            if ($(".topbar").offset().top > 50) {
                $(".topbar").addClass("bgwhite padd-10");
-               document.getElementById('logo').setAttribute("src","/images/logo_blue.png");
+               document.getElementById('logo').setAttribute("src","/logo_blue.png");
            } else {
                $(".topbar").removeClass("bgwhite padd-10");
-               document.getElementById('logo').setAttribute("src","/images/logo_white.png");
+               document.getElementById('logo').setAttribute("src","/logo_white.png");
            }
        });
 
